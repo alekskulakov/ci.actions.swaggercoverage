a2605082606a7fb5c852ea3badc0b614ef221f10
Aleksandr Kulakov <a.kulakov@dodobrands.io>
2025-03-11 16:04:02 +0500

    update template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-﻿process.argv.forEach((val, index) => {
-    console.log(index + ': ' + val);
-});
+﻿const args = process.argv.slice(2);
+
+const baseFile = args[0];
+const resultsFile = args[1];
+const isDefaultBranch = args[2];
+
+const fs = require('fs');
+const core = require('@actions/core');
+
+let threshold = 0;
+let fullPercentage = 0;
+
+if (!fs.existsSync(baseFile)) {
+    core.info(`File ${baseFile} not found!`);
+} else {
+    threshold = fs.readFileSync(baseFile, "utf8");
+}
+core.info(`Base threshold: ${threshold}`);
