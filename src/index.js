8a7e1b0036708a6961a4f495a8a6287ba301368d
Aleksandr Kulakov <a.kulakov@dodobrands.io>
2025-03-11 16:49:24 +0500

    update template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 ﻿const args = process.argv.slice(2);
 
-const baseFile = 'coverage-threshold.txt';
-const baseFile2 = './coverage-threshold.txt';
+const baseFile = args[0];
 const resultsFile = args[1];
 const isDefaultBranch = args[2];
 
@@ -9,13 +8,6 @@ const fs = require('fs');
 
 let threshold = 0;
 let fullPercentage = 0;
-
-console.log('Current working directory:', process.cwd());
-console.log('Script directory:', __dirname);
-
-if (!fs.existsSync(baseFile2)) {
-    console.log(`baseFile2 ${baseFile2} not found!`);
-}
 
 if (!fs.existsSync(baseFile)) {
     console.log(`File ${baseFile} not found!`);
